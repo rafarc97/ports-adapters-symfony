eff16917ba4dc49bb819e3d0372c9d8cff8f01c5
DESKTOP-K0GGD3A\rafae <rafa.rodricalve@alum.uca.es>
2024-03-24 09:50:26 +0100

    application layer template created

---
+++ src/orders/list/application/response/OrdersResponse.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\orders\list\application\response;
+
+use App\orders\list\domain\Order;
+
+final readonly class OrdersResponse
+{
+    private array $orders;
+
+    public function __construct(OrderResponse ...$orders)
+    {
+        $this->orders = $orders;
+    }
+
+    public function orders(): array
+    {
+        return $this->orders;
+    }
+}
